f2c3527347e533d9ac3a66f185df9b1a9e14a08c
alecnissen <alecnissen@yahoo.com>
2023-05-19 15:30:13 -0400

    added html form structure, now will work on styling the html elements

---
+++ main.js
@@ -0,0 +1,74 @@
+// gameplan 
+
+// use the same set-up as before, 
+
+// check it's input field using conditionals 
+
+// like before with library, 
+
+// ensure each field is properly filled in, 
+
+// look up how to properly create a form 
+
+// adding classes with an error, 
+
+// removing classes/styling when everything is ok with no errors in input 
+
+// apply a background too, with the form contained within a 
+
+// larger container, 
+
+// form in center, 
+
+// email 
+
+// county 
+
+// build the outline, 
+
+// might be a look idea to watch another video on form 
+
+// validation, just to see different methods and to get different 
+
+// perspectives and ideas. 
+
+
+
+
+// whiteboard, 
+
+// build the form, 
+
+// apply a background color to the body 
+
+// have the form be wrapped into a container, 
+
+// style the forms container, 
+
+// add the right input fields, email, country, zip code 
+
+// password and password confirmation fields, 
+
+// live inline validation meaning, that will listen 
+
+// for a change event, or if the user clicks out of the input field, 
+
+// try to make the an input field, and play around with change event 
+
+// I will use a change event listener, listeners for any input, 
+
+// it will highlight red and show error message until requirement is met, 
+
+// all vaidation within a change listener, 
+
+// as well as the submit btn, 
+
+// submit btn will need a listeners too that will basically have the same logic 
+
+// submit btn checks for errors, displays errors and styling 
+
+// as well as change event, highlights red and shows errors 
+
+// until requirement is met 
+
+
