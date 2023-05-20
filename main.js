5129a1ddeef952cef01cfcbc878eb3cb677fda36
alecnissen <alecnissen@yahoo.com>
2023-05-19 20:03:44 -0400

    started adding form validation, working on if else blocks, blank fields will work on the conditional and same with length > than 5 but having difficulty with .com and @ symbols

--- main.js
+++ main.js
@@ -1,3 +1,142 @@
+// grab all input values, 
+
+// input fields grabbed, 
+
+// create input listener for each field, 
+
+// work conditionals inside there, 
+
+// first email, and first make sure email is not blank, 
+
+// then apply error class, 
+
+
+let emailInput = document.getElementById("email-input");
+
+let countryInput = document.getElementById("country-input"); 
+
+let zipCodeInput = document.getElementById("zip-code-input"); 
+
+let passwordInput = document.getElementById("password-input"); 
+
+let passwordInputConfirmation = document.getElementById("password-input-confirmation"); 
+
+let errorClassEmail = document.getElementsByClassName("error")[0]; 
+ 
+// console.log(errorClassEmail);
+
+// let errorMessagesArr = []; 
+
+emailInput.addEventListener('input', (e) => {
+
+
+    let val = e.target.value;
+
+    if (val === '' || val == null) {
+         errorClassEmail.textContent = "Email cannot be blank!";
+        // emailInput.classList.add('error-input');
+         emailInput.style.borderColor = "red";
+    } else if (val.length < 5) {
+         errorClassEmail.textContent = "Email cannot be less than 5 characters"; 
+        //  emailInput.classList.add('error-input');
+          emailInput.style.borderColor = "red";
+    } else if (val.length > 5) {
+        errorClassEmail.textContent = '';
+        // emailInput.style.borderColor = '#e7e2e2';
+    } else if (!val.includes('@')) {
+        errorClassEmail.textContent = "Must include @";
+        // emailInput.style.borderColor = "red"; 
+    } 
+})
+
+
+// if (errorMessagesArr.length > 0) { 
+//     errorClassEmail.append(errorMessagesArr).join(", ");
+//     // errorClassEmail.textContent = errorMessagesArr.join(", ");
+
+// }
+
+// console.log(errorMessagesArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // gameplan 
 
 // use the same set-up as before, 
@@ -70,5 +209,50 @@
 // as well as change event, highlights red and shows errors 
 
 // until requirement is met 
+
+
+// have the html and the css intact and I like the look 
+
+// now I will need to grab each input field, 
+
+// use an input event listener, and within each event listener, 
+
+// I will use conditionals within the listener to check validation, 
+
+// if condition is met then apply error class, or success class, 
+
+// I will apply the success class or the error, class 
+
+// work on the same logic for submit event, 
+
+// you could probably apply the same styles. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
