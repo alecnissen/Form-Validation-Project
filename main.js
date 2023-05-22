722548750d370240e7ff52ed1ad4f2fbaae2ec28
alecnissen <alecnissen@yahoo.com>
2023-05-22 15:22:18 -0400

    refactored, using check inputs for each field then each field will have a function which sets error message and success message, will work on password confirmation last

--- main.js
+++ main.js
@@ -22,93 +22,211 @@ let passwordInput = document.getElementById("password-input");
 let passwordInputConfirmation = document.getElementById("password-input-confirmation"); 
 
 let errorClassEmail = document.getElementsByClassName("error")[0]; 
- 
-// console.log(errorClassEmail);
 
-// let errorMessagesArr = []; 
+let errorClassCountry = document.getElementsByClassName("error")[1]; 
+
+let errorClassZipCode = document.getElementsByClassName("error")[2]; 
+
+console.log(errorClassZipCode);
+
+let errorMessage;
+
+let val;
 
 emailInput.addEventListener('input', (e) => {
 
+     val = e.target.value;
 
-    let val = e.target.value;
+    // pass the values to a function which prints the message. 
 
-    if (val === '' || val == null) {
-         errorClassEmail.textContent = "Email cannot be blank!";
-        // emailInput.classList.add('error-input');
-         emailInput.style.borderColor = "red";
-    } else if (val.length < 5) {
-         errorClassEmail.textContent = "Email cannot be less than 5 characters"; 
-        //  emailInput.classList.add('error-input');
-          emailInput.style.borderColor = "red";
-    } else if (val.length > 5) {
-        errorClassEmail.textContent = '';
-        // emailInput.style.borderColor = '#e7e2e2';
-    } else if (!val.includes('@')) {
-        errorClassEmail.textContent = "Must include @";
-        // emailInput.style.borderColor = "red"; 
-    } 
+    checkEmailInput(val);
+
+    // if (val === '' || val == null) {
+    //     errorMessage = "Email cannot be blank!";
+
+    //     setErrorClass(val, errorMessage);
+
+
+    //     // errorClassEmail.textContent = "Email cannot be blank!";
+    //     //  emailInput.classList.add('error-input');
+    //     // emailInput.className = "error-input";
+    //     //  emailInput.style.borderColor = "red";
+    // } else if (val.length < 5) {
+
+    //     errorMessage = "Email cannot be less than 5 characters"; 
+
+    //     setErrorClass(val, errorMessage); 
+    //      // errorClassEmail.textContent = "Email cannot be less than 5 characters"; 
+    //     //  emailInput.classList.add('error-input');
+    //     // emailInput.className = "error-input";
+    //      //  emailInput.style.borderColor = "red";
+    // } else if (!val.contains('@')) {
+    //     errorMessage = "Must include @";
+
+    //     setErrorClass(val, errorMessage); 
+    //     // errorClassEmail.textContent = "Must include @";
+    //     // emailInput.style.borderColor = "red"; 
+    // } 
+
+
+}) 
+
+
+countryInput.addEventListener("input", (e) => { 
+
+    val = e.target.value; 
+
+    checkCountryInput(val);
+
+}); 
+
+zipCodeInput.addEventListener("input", (e) => { 
+
+    val = e.target.value;  
+
+    checkZipCodeInput(val);
+
 })
 
 
-// if (errorMessagesArr.length > 0) { 
-//     errorClassEmail.append(errorMessagesArr).join(", ");
-//     // errorClassEmail.textContent = errorMessagesArr.join(", ");
+function checkEmailInput(input) { 
+    let inputValue = input; 
 
-// }
+    if (inputValue === '' || inputValue == null) { 
+        setErrorClassEmail(inputValue, "Email cannot be blank!");        
+    } else if (!isValidEmail(inputValue)) { 
+        setErrorClassEmail(inputValue, "Provide a valid email!");
+    } else { 
+        setSuccessClassEmail();
+    } 
 
-// console.log(errorMessagesArr);
+    
+} 
 
+function checkCountryInput(input) { 
 
+    let inputValue = input; 
+    
+    if (inputValue === '' || inputValue == null) { 
+        setErrorClassCountry(inputValue, "Country cannot be blank!")
+    } else if (inputValue.length > 25) { 
+        setErrorClassCountry(inputValue, "Exceeds length of 25 characters!"); 
+    } else if (inputValue.length < 2) { 
+        setErrorClassCountry(inputValue, "Cannot be less than 2 characters!"); 
+    } else { 
+        setSuccessClassCountry()
+    }
+} 
 
+function checkZipCodeInput(input) { 
 
+    let inputValue = input; 
 
+    if (inputValue === '' || inputValue == null) { 
+        setErrorClassZipCode(inputValue, "Zip-Code cannot be blank!");
+    } else if (inputValue.length < 5) { 
+        setErrorClassZipCode(inputValue, "Zip must be 5 characters!")
+    } else if (!Number(inputValue)) { 
+        setErrorClassZipCode(inputValue, "Zip must be a number!");
+    } else { 
+        setSuccessZipCode();
+    }
 
+}
 
 
+// swipe out the last else if block is use regex format to check, 
 
+// that way it covers both @format and other email format 
 
 
+function isValidEmail(email) { 
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 
 
 
+function setErrorClassEmail(value, message) { 
+    let theValue = value; 
 
+    let theMessage = message; 
 
+    errorClassEmail.textContent = theMessage; 
 
+    emailInput.style.border = "2px solid red";
 
+} 
 
 
+function setSuccessClassEmail() { 
+    emailInput.style.border = "2px solid green";
+    errorClassEmail.textContent = '';
+}
 
 
+function setErrorClassCountry(value, message) { 
+    let theValue = value; 
 
+    let theMessage = message; 
 
+    errorClassCountry.textContent = theMessage; 
 
+    countryInput.style.border = "2px solid red";
 
+} 
 
+function setSuccessClassCountry() { 
+    
+    countryInput.style.border = "2px solid green";
 
+    errorClassCountry.textContent = ''; 
+ 
+} 
 
+function setErrorClassZipCode(value, message) { 
+    let theValue = value; 
 
+    let theMessage = message;  
 
+    errorClassZipCode.textContent = theMessage;
 
+    zipCodeInput.style.border = "2px solid red";
 
+} 
 
+function setSuccessZipCode() { 
 
+    zipCodeInput.style.border = "2px solid red";
 
+    errorClassZipCode.textContent = '';
 
+}
 
 
 
+// one function which will check inputs, 
 
+// function will take those inputs, any apply the appropriate message to them, 
 
+// either apply the error class or the success class, 
 
+// 
 
 
 
 
+// pass the value, and message to changeMessage function, 
 
+// should the value and message variables be global? 
 
+// should I make them as general as possible, 
 
+// within the global scope, then within each input listener for each field, 
 
+// values will be updated and passed the functons, 
 
+// try the first one with email, 
 
 
 
@@ -137,95 +255,46 @@ emailInput.addEventListener('input', (e) => {
 
 
 
-// gameplan 
 
-// use the same set-up as before, 
 
-// check it's input field using conditionals 
 
-// like before with library, 
 
-// ensure each field is properly filled in, 
 
-// look up how to properly create a form 
 
-// adding classes with an error, 
 
-// removing classes/styling when everything is ok with no errors in input 
 
-// apply a background too, with the form contained within a 
 
-// larger container, 
 
-// form in center, 
 
-// email 
 
-// county 
 
-// build the outline, 
 
-// might be a look idea to watch another video on form 
 
-// validation, just to see different methods and to get different 
 
-// perspectives and ideas. 
 
 
 
 
-// whiteboard, 
 
-// build the form, 
 
-// apply a background color to the body 
 
-// have the form be wrapped into a container, 
 
-// style the forms container, 
 
-// add the right input fields, email, country, zip code 
 
-// password and password confirmation fields, 
 
-// live inline validation meaning, that will listen 
 
-// for a change event, or if the user clicks out of the input field, 
 
-// try to make the an input field, and play around with change event 
 
-// I will use a change event listener, listeners for any input, 
 
-// it will highlight red and show error message until requirement is met, 
 
-// all vaidation within a change listener, 
 
-// as well as the submit btn, 
 
-// submit btn will need a listeners too that will basically have the same logic 
 
-// submit btn checks for errors, displays errors and styling 
 
-// as well as change event, highlights red and shows errors 
 
-// until requirement is met 
 
 
-// have the html and the css intact and I like the look 
 
-// now I will need to grab each input field, 
-
-// use an input event listener, and within each event listener, 
-
-// I will use conditionals within the listener to check validation, 
-
-// if condition is met then apply error class, or success class, 
-
-// I will apply the success class or the error, class 
-
-// work on the same logic for submit event, 
-
-// you could probably apply the same styles. 
 
 
 
